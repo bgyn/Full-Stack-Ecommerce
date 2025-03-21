6b0fcd6027ff1d188f3eaf13573fe7fd8bdcfd87
Bigyan Thanait <bigyanthanait8@gmail.com>
2025-03-21 13:16:14 +0545

    remove .js file build during the tsc build

--- backend/src/routes/reviewRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const authMiddleware_1 = require("../middelware/authMiddleware");
-const reviewController_1 = require("../controllers/reviewController");
-const router = (0, express_1.Router)();
-router.get("/", (0, authMiddleware_1.restrictTo)(["ADMIN", "USER"]), reviewController_1.getReviews);
-exports.default = router;
--- backend/src/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const authMiddleware_1 = require("../middelware/authMiddleware");
-const userController_1 = require("../controllers/userController");
-const router = (0, express_1.Router)();
-router.get("/", (0, authMiddleware_1.restrictTo)(["ADMIN"]), userController_1.getUsers);
-exports.default = router;
